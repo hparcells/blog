a17fd41b72f5be4a0cf6eb910bb31f6c395f7d7c
Hunter Parcells <hunterparcells@gmail.com>
2023-03-28 13:20:35 -0400

    Added unlisted option for posts.

--- src/lib/markdoc/frontmatter.schema.ts
+++ src/lib/markdoc/frontmatter.schema.ts
@@ -1,8 +1,6 @@
 import { z } from "zod";
 
 const baseSchema = z.object({
-  draft: z.boolean().default(false),
-  featured: z.boolean().default(false),
   title: z.string({
     required_error: "Required frontmatter missing: title",
     invalid_type_error: "title must be a string",
@@ -10,8 +8,10 @@ const baseSchema = z.object({
   date: z.date({
     required_error: "Required frontmatter missing: date",
     invalid_type_error:
-      "date must be written in yyyy-mm-dd format without quotes: For example, Jan 22, 2000 should be written as 2000-01-22.",
+    "date must be written in yyyy-mm-dd format without quotes: For example, Jan 22, 2000 should be written as 2000-01-22.",
   }),
+  draft: z.boolean().default(false),
+  unlisted: z.boolean().default(false)
 });
 
 /*
